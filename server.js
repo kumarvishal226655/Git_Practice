1a5b748833265f3b290f6bcb676a7d68375677b4
Kumarvishal22665@ <kumarvishal22665@icloud.com>
2026-03-02 16:09:24 +0530

    Add authontication routes

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/authRoutes.js";
+
+
+
 dotenv.config();
 
 const app = express();
@@ -14,6 +18,8 @@ mongoose.connect(process.env.MONGO_URI)
 app.get("/", (req, res) => {
   res.send("Library API Running 🚀");
 });
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
