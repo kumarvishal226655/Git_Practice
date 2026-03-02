46510a023b2e1892befe30d145244b5be7c13458
Kumarvishal22665@ <kumarvishal22665@icloud.com>
2026-03-02 15:54:21 +0530

    Add User Model with Password hashing

---
+++ models/User.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    roll:{
+        type:String,
+        enum:["admin", "user",],
+        default: "user"
+    },
+},
+{timestamps:true}
+
+);
+
+// Hassing password before saving 
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+});
+
+export default mongoose.model("User",userSchema);
